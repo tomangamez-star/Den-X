71a02028fdbbf54fd91316914ba82e024fb2c7e6
tomangamez-star <tomangamez@gmail.com>
2026-09-09 09:39:04 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "denx-animator-v036-tool-identity-color-studio";
+const CACHE_NAME = "denx-animator-v038-cumulative-color-figure-label-fix";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -57,45 +57,5 @@ const APP_SHELL = [
   "./icons/tools/add-figure.svg",
   "./icons/tools/camera.svg",
   "./icons/tools/front.svg",
-  "./icons/tools/back.svg"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME)
-          .then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request)
-          .then(cached =>
-            cached || caches.match("./index.html")
-          )
-      )
-  );
-});
+  "./icons/tools/back.svg",
+  "./icons/tools/nav-back.svg"
