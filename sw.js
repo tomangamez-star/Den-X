8394ea50eb64a099761d5f07d3ac38862c7362ec
tomangamez-star <tomangamez@gmail.com>
2026-09-10 11:19:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "denx-animator-v045-toolbox-rebuild";
+const CACHE_NAME = "denx-animator-v046-virtual-toolbox";
 
 const APP_SHELL = [
   "./",
@@ -16,11 +16,11 @@ const APP_SHELL = [
   "./css/v0.3.3-quickdeck.css",
   "./css/v0.3.4-stability.css",
   "./css/tool-identity.css",
-  "./css/toolbox-rebuild-v045.css",
+  "./css/toolbox-rebuild-v046.css",
 
   "./js/project-store.js",
   "./js/app.js",
-  "./js/toolbox-rebuild-v045.js",
+  "./js/toolbox-rebuild-v046.js",
   "./js/figure-copy-paste-v044.js",
   "./js/home-projects.js",
   "./js/open-projects.js",
