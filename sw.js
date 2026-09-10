e94e23a0fce657f7c6328f7dd3d13037eb5a0370
tomangamez-star <tomangamez@gmail.com>
2026-09-10 21:41:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "denx-animator-v053-frame-local";
+const CACHE_NAME = "denx-animator-v054-frame-local-core";
 
 const APP_SHELL = [
   "./",
@@ -16,14 +16,16 @@ const APP_SHELL = [
   "./css/v0.3.3-quickdeck.css",
   "./css/v0.3.4-stability.css",
   "./css/tool-identity.css",
-  "./css/frame-local-v053.css",
+  "./css/frame-local-v054.css",
   "./css/toolbox-rebuild-v052.css",
   "./css/webgl-renderer-v2.css",
 
   "./js/project-store.js",
   "./js/app.js",
+  "./js/frame-local-core-v054.js",
+  "./js/operation-progress-v054.js",
+  "./js/settings-diagnostics-v054.js",
   "./js/workspace-boot-v053.js",
-  "./js/frame-local-v053.js",
   "./js/toolbox-rebuild-v052.js",
   "./js/viewport-guard-v052.js",
   "./js/webgl-renderer-v2.js",
